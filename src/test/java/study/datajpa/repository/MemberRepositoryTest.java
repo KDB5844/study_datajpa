6941243821029e6b64d56edb211fb02219e73559
Subin Cho <sbjo7713@gmail.com>
2022-06-22 00:22:11 +0900

    EntityGraph 알아보기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -248,7 +248,7 @@ class MemberRepositoryTest {
         teamRepository.save(teamB);
 
         Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 10, teamB);
+        Member member2 = new Member("member1", 10, teamB);
         memberRepository.save(member1);
         memberRepository.save(member2);
 
@@ -256,7 +256,7 @@ class MemberRepositoryTest {
         em.clear();
 
         // when
-        List<Member> members = memberRepository.findMemberFetchJoin();
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
 
         for (Member member : members) {
             System.out.println("member = " + member.getTeam());
